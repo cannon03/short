14cb5086d9ab4f0dc73924f56b599b447fbdd99c
shourya <shourya>
2024-05-06 00:53:53 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+express = require("express");
+mongoose = require("mongoose");
+
+const PORT = 5000;
+
+app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  res.render("index", {});
+});
+
+app.post("/shorten", async (req, res) => {
+  const url = req.body.InputURL;
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
+  console.log(url);
+  console.log(ip);
+  res.render("shorten", {
+    url: url,
+    IP: ip,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+});
