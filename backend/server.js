d0fcb8fe007cda5b02439b355014a622620556db
shourya <shourya>
2024-05-06 01:11:19 +0530

    ip

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get("/", async (req, res) => {
 
 app.post("/shorten", async (req, res) => {
   const url = req.body.InputURL;
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
+  const ip = req.headers["x-forwarded-for"];
   console.log(url);
   console.log(ip);
   res.render("shorten", {
